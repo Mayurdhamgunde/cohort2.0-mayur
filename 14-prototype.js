a5ab801ddf2a8380a8d342778c79d1d8de0dba4e
mayur121-cmd <mayurdhamgunde@gmail.com>
2026-03-01 00:05:57 +0530

    Add prototype

--- 14-prototype.js
+++ 14-prototype.js
@@ -31,7 +31,20 @@ console.log(ranbir.runBusiness());
 console.log(ranbir.cookTraditionalDish());          // This demonstrates how the ranbir object can access methods from both its parent (raj) and grandparent (prithviraj) objects, showcasing the power of prototypal inheritance in JavaScript.
 
 
+Array.prototype.last = function (){    // Here, we are adding a new method called last to the Array prototype. This means that all arrays in JavaScript will now have access to this method.
+    return this[this.length - 1];       // The last method returns the last element of the array by accessing the element at the index of length - 1. This allows us to easily retrieve the last item from any array without having to manually calculate the index each time.
+} // By adding the last method to the Array prototype, we can now call it on any array instance, as shown in the example below. This demonstrates how we can extend the functionality of built-in objects in JavaScript using prototypes.
 
+Array.prototype.mayur = "mayur";
 
+String.prototype.mayur = "mayur";
 
+console.log([1,2,3,4,5,10].last()); // This will output 10, which is the last element of the array. The last method is now available to all arrays, allowing us to easily access the last item without needing to write additional code for each array.
+console.log(["Ani","hitesh","Akash"].last());
 
+/*
+
+NOTE : - 
+ polyfill is a piece of code (usually JavaScript on the Web) used to provide modern functionality on older browsers that do not natively support it. It allows developers to use newer features of JavaScript while ensuring compatibility with older browsers. In the example above, we added a new method called last to the Array prototype, which is a form of polyfill. This means that even if the last method is not natively supported in older browsers, it will still work as expected when called on any array instance.
+
+*/
